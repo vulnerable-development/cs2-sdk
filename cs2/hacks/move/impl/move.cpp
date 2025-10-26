406dc6294bc54ec6bd1bbec20c10c08047c36d2e
anim_state <82110489+lagcompensation@users.noreply.github.com>
2025-10-26 10:31:49 +0200

    hotfix

--- cs2/hacks/move/impl/move.cpp
+++ cs2/hacks/move/impl/move.cpp
@@ -1,7 +1,7 @@
 #include "../../../cs2.hpp"
 
 namespace hacks {
-    void c_move::run( valve::user_cmd_t* const cmd ) const {
+    void c_move::run( valve::user_cmd_t* const cmd ) {
         const auto move_type = g_local_player->pawn( )->move_type( );
         if ( move_type == valve::e_move_type::noclip
             || move_type == valve::e_move_type::ladder )
@@ -12,17 +12,15 @@ namespace hacks {
 
         if ( cfg.m_bhop.get( )
             && !( cvars.sv_autobunnyhopping->as< bool >( ) ) ) {
-            static bool should_fake_jump{};
-
-            if ( should_fake_jump ) {
+            if ( m_should_fake_jump ) {
                 cmd->m_buttons.m_pressed |= valve::e_buttons::in_jump;
-                should_fake_jump = false;
+                m_should_fake_jump = false;
             }
             else if ( cmd->m_buttons.m_pressed & valve::e_buttons::in_jump ) {
                 if ( g_local_player->pawn( )->flags( ) & valve::e_ent_flags::on_ground )
                     cmd->m_buttons.m_pressed &= ~valve::e_buttons::in_jump;
                 else
-                    should_fake_jump = true;
+                    m_should_fake_jump = true;
             }
         }
     }
--- cs2/hacks/move/move.hpp
+++ cs2/hacks/move/move.hpp
@@ -2,8 +2,10 @@
 
 namespace hacks {
     class c_move {
+    private:
+        bool m_should_fake_jump{};
     public:
-        void run( valve::user_cmd_t* const cmd ) const;
+        void run( valve::user_cmd_t* const cmd );
     };
 
     inline const auto g_move = std::make_unique< c_move >( );
